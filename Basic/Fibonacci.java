fc9822aa503516741b05db39ae34d6f8a314b199
rheaannboby <57282344+rheaannboby@users.noreply.github.com>
2023-03-09 02:50:59 +0530

    Update Fibonacci.java

--- Basic/Fibonacci.java
+++ Basic/Fibonacci.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter the limit: ");
+		int lim = sc.nextInt();
+		int fib0 = 0, fib1 = 1;
+		int fibcurr = fib0 + fib1;
+		System.out.print("The fibonacci series till "+lim+" is :\n"+fib0+" "+fib1+" ");
+		for(;fibcurr <= lim;) {
+			System.out.print(fibcurr+" ");
+			fib0 = fib1;
+			fib1 = fibcurr;
+			fibcurr = fib0 + fib1;
+		}
+
+	}
+
+}
 
